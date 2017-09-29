d9144c87119e91635d1f6edf478025131398de28
juanfe428 <juanfelipesandoval2000@gmail.com>
2017-09-29 17:04:27 -0500

    1.1

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from wtforms import Form
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
+from models import User
 
 
 class CommentForm(Form):
@@ -12,6 +13,14 @@ class CommentForm(Form):
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm=PasswordField('')
+
+    def validate_username(form,field ):
+    	username=field.data 
+    	user=User.query.filter_by(username=username).first()
+    	if user is not None:
+    		raise validators.ValidationError("El usuario esta registrado")
+
+
 class LoginForm(Form):
 	username=StringField('',[validators.length(min=4,max=25,message='Igrese un username valido')])
 	password =PasswordField('',[validators.DataRequired()])
